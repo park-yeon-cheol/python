dcda4ed4fe8ac3910c3f99b75e92ef17768ef579
park-yeon-cheol <qkr33291119@gmail.com>
2021-06-16 08:55:36 +0900

    beakjoon

--- 9625.py
+++ 9625.py
@@ -26,3 +26,32 @@ result_B=len(a)-result_A
 print(result_A,result_B)
 
 #firt_try: 처음 입력이 A이기 때문에 배열에 넣어 두고 change 라는 함수를 만들었다. change 함수는 A이면 B로 바꾸고 B 이면 BA로 바꾸는 함수이다. 그 후 A와 B의 갯수를 count 하여 출력하였는데 시간 초과가 나왔다.
+
+# 1 = 0 1
+# 2 = 1 1
+# 3 = 1 2
+# 4 = 2 3
+
+# 0 1 1 2 3 5 8 13 ...
+#fib(0) = 0
+#fib(1) = 1
+#fib(2) = 1
+#fib(3) = 2
+# => fib(n-1), fib(n)
+
+def fibo(n):
+    result=0
+    a=0
+    b=1
+    if n<2:
+        return n
+    else:
+        for i in range(1,n):
+            result=a+b
+            a=b
+            b=result
+        return result
+k=int(input())
+print(fibo(k-1),fibo(k))
+
+#규칙을 찾아보니 피보나치가 나왔다. k일때 fib(k-1), fib(k)가 나와서 피보나치 함수를 작성하여 구하였다.
